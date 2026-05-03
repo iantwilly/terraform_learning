4e0e2cf9529a1ab94d826c276f5e3a1e4ffde079
iantwilly <ianterrywilly@gmail.com>
2026-05-03 16:17:03 +0530

    main updated

--- L5/main.tf
+++ L5/main.tf
@@ -4,7 +4,8 @@ terraform {
       source  = "hashicorp/aws"
       version = "6.43.0"
     }
-
+  }
+  required_providers {
     tls = {
       source  = "hashicorp/tls"
       version = "4.2.1"
@@ -16,47 +17,47 @@ provider "aws" {
   region = var.region
 }
 data "aws_ami" "ubuntu" {
-  most_recent = true
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-  owners = ["099720109477"]
+    most_recent = true
+ filter {
+    name = "name"
+    values =["ubuntu/ubuntu/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+ }  
+ filter {
+    name = "virtualization-type"
+    values =["hvm"]
+ }
+  owners = [ "099720109477" ]
 }
 resource "tls_private_key" "mykey" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-
+    algorithm = "RSA"
+    rsa_bits =4096 
+  
 }
 resource "aws_key_pair" "mykey" {
-  key_name   = "mykey.pem"
-  public_key = tls_private_key.mykey.public_key_openssh
-
+    key_name = "mykey.pem"
+    public_key = tls_private_key.mykey.public_key_openssh
+  
 }
 resource "aws_instance" "webserver" {
-  ami                    = data.aws_ami.ubuntu.id
-  instance_type          = var.my_instnace_type
-  key_name               = aws_key_pair.mykey.key_name
-  vpc_security_group_ids = [aws_security_group.security_group1.id]
+    ami=data.aws_ami.ubuntu.id
+    instance_type = var.my_instnace_type
+    key_name = aws_key_pair.mykey.key_name
+    vpc_security_group_ids = [ aws_security_group.security_group1.id ] 
 }
 resource "aws_security_group" "security_group1" {
 
-  ingress {
-    cidr_blocks = ["0.0.0.0/0"]
-    description = "SSH Ingress"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-  }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
+    ingress{
+        cidr_blocks = ["0.0.0.0/0"]
+        description = "SSH Ingress"
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+    }
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+  
 }
